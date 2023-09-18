f29142259294499c3541e3481c8adb025b9e76cf
knichu <leonard1219@naver.com>
2023-09-18 18:21:08 +0900

    refactor : RequestDTO 수정

--- data/src/main/java/com/knichu/data/dto/request/LiveWeatherRequestDTO.kt
+++ data/src/main/java/com/knichu/data/dto/request/LiveWeatherRequestDTO.kt
@@ -3,6 +3,8 @@ package com.knichu.data.dto.request
 import com.google.gson.annotations.SerializedName
 
 data class LiveWeatherRequestDTO(
+    @field:SerializedName("serviceKey") val serviceKey: String? = null,
+    @field:SerializedName("pageNo") val pageNo: Long? = null,
     @field:SerializedName("numOfRows") val numOfRows: Long? = null,
     @field:SerializedName("dataType") val dataType: String? = null,
     @field:SerializedName("base_date") val baseDate: Long? = null,
