af8076515415cde525cd23b08ca07fb418ab10ba
knichu <leonard1219@naver.com>
2023-09-20 17:55:06 +0900

    refactor : 빠진 nullable 추가

--- data/src/main/java/com/knichu/data/dto/response/entity/shortWeather/SWHeader.kt
+++ data/src/main/java/com/knichu/data/dto/response/entity/shortWeather/SWHeader.kt
@@ -3,6 +3,6 @@ package com.knichu.data.dto.response.entity.shortWeather
 import com.google.gson.annotations.SerializedName
 
 data class SWHeader(
-    @field:SerializedName("resultCode") val resultCode: String,
-    @field:SerializedName("resultMsg") val resultMsg: String
+    @field:SerializedName("resultCode") val resultCode: String? = null,
+    @field:SerializedName("resultMsg") val resultMsg: String? = null
 )
--- data/src/main/java/com/knichu/data/dto/response/entity/shortWeather/SWResponse.kt
+++ data/src/main/java/com/knichu/data/dto/response/entity/shortWeather/SWResponse.kt
@@ -3,6 +3,6 @@ package com.knichu.data.dto.response.entity.shortWeather
 import com.google.gson.annotations.SerializedName
 
 data class SWResponse(
-    @field:SerializedName("header") val sWHeader: SWHeader,
+    @field:SerializedName("header") val sWHeader: SWHeader? = null,
     @field:SerializedName("body") val sWBody: SWBody? = null
 )
