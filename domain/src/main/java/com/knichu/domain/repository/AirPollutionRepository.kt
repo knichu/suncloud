cd4750d65cee0e3a467a45ee40d9d9b225f4cefa
knichu <leonard1219@naver.com>
2023-11-02 18:28:04 +0900

    refactor : repository 수정

--- domain/src/main/java/com/knichu/domain/repository/AirPollutionRepository.kt
+++ domain/src/main/java/com/knichu/domain/repository/AirPollutionRepository.kt
@@ -5,5 +5,5 @@ import com.knichu.domain.vo.AirPollutionVO
 import io.reactivex.rxjava3.core.Single
 
 interface AirPollutionRepository {
-    fun getAirPollution(param: AirPollutionRequestParam): Single<AirPollutionVO>
+    fun getAirPollution(param: AirPollutionRequestParam): Result<Single<AirPollutionVO>>
 }
