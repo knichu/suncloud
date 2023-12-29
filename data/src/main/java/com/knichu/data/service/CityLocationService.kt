3ff25ffdf7264ff2370ac6a61606b66f3383fee1
knichu <leonard1219@naver.com>
2023-12-29 16:03:07 +0900

    feat : CityLocationDataSource, CityLocationService 추가

---
+++ data/src/main/java/com/knichu/data/service/CityLocationService.kt
@@ -0,0 +1,12 @@
+package com.knichu.data.service
+
+import com.knichu.data.dto.response.CityLocationResponseDTO
+import io.reactivex.rxjava3.core.Single
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface CityLocationService {
+    @GET("json/cityCode.json")
+    fun getAllCityLocation(): Single<Response<CityLocationResponseDTO>>
+}
+
