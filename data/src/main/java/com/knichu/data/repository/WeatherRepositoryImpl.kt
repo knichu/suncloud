7fe755997611032047d93f3680a8a572194160c1
knichu <leonard1219@naver.com>
2024-01-31 23:03:59 +0900

    refactor : MidWeather 생성
    ShortWeather를 short, mid로 분리

--- data/src/main/java/com/knichu/data/repository/WeatherRepositoryImpl.kt
+++ data/src/main/java/com/knichu/data/repository/WeatherRepositoryImpl.kt
@@ -4,17 +4,20 @@ import com.knichu.data.datasource.WeatherDataSource
 import com.knichu.data.dto.request.LiveWeatherRequestDTO
 import com.knichu.data.dto.request.LongRainCloudRequestDTO
 import com.knichu.data.dto.request.LongTemperatureRequestDTO
+import com.knichu.data.dto.request.MidWeatherRequestDTO
 import com.knichu.data.dto.request.ShortWeatherRequestDTO
 import com.knichu.data.dto.request.WeatherForecastTextRequestDTO
 import com.knichu.domain.param.LiveWeatherRequestParam
 import com.knichu.domain.param.LongRainCloudRequestParam
 import com.knichu.domain.param.LongTemperatureRequestParam
+import com.knichu.domain.param.MidWeatherRequestParam
 import com.knichu.domain.param.ShortWeatherRequestParam
 import com.knichu.domain.param.WeatherForecastTextRequestParam
 import com.knichu.domain.repository.WeatherRepository
 import com.knichu.domain.vo.LiveWeatherVO
 import com.knichu.domain.vo.LongRainCloudVO
 import com.knichu.domain.vo.LongTemperatureVO
+import com.knichu.domain.vo.MidWeatherVO
 import com.knichu.domain.vo.ShortWeatherVO
 import com.knichu.domain.vo.WeatherForecastTextVO
 import io.reactivex.rxjava3.core.Single
@@ -59,6 +62,19 @@ class WeatherRepositoryImpl @Inject constructor(
             .subscribeOn(Schedulers.io())
             .map { responseDTO -> responseDTO.toDomain() }
             .onErrorReturn { ShortWeatherVO(item = emptyList()) }
+    }
+
+    override fun getMidWeather(param: MidWeatherRequestParam): Single<MidWeatherVO> {
+        return weatherDataSource.getMidWeather(
+            MidWeatherRequestDTO(
+                baseDate = param.baseDate,
+                nx = param.nx,
+                ny = param.ny
+            )
+        )
+            .subscribeOn(Schedulers.io())
+            .map { responseDTO -> responseDTO.toDomain() }
+            .onErrorReturn { MidWeatherVO(item = emptyList()) }
     }
 
     override fun getLongRainCloud(param: LongRainCloudRequestParam): Single<LongRainCloudVO> {
