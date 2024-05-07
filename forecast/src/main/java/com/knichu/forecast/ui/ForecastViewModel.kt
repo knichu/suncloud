9d957da5a2830ae634755aca410a02d873fbf0b9
knichu <leonard1219@naver.com>
2024-05-07 20:54:30 +0900

    feat : ForecastViewModel 설계
    DataStore, AirPollution 관련 미리 추가
    OtherInfo, ForecastText Data 미리 추가

--- forecast/src/main/java/com/knichu/forecast/ui/ForecastViewModel.kt
+++ forecast/src/main/java/com/knichu/forecast/ui/ForecastViewModel.kt
@@ -1,9 +1,207 @@
 package com.knichu.forecast.ui
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.knichu.common.base.BaseViewModel
+import com.knichu.common_ui.lifecycle.SingleLiveEvent
+import com.knichu.domain.usecase.AirPollutionUseCase
+import com.knichu.domain.usecase.DataStoreUseCase
+import com.knichu.domain.usecase.WeatherUseCase
+import com.knichu.domain.vo.SunriseSunsetVO
+import com.knichu.domain.vo.Weather24HourItemVO
+import com.knichu.domain.vo.Weather24HourVO
+import com.knichu.domain.vo.WeatherForecastTextVO
+import com.knichu.domain.vo.WeatherNowVO
+import com.knichu.domain.vo.WeatherOtherInfoVO
+import com.knichu.domain.vo.WeatherWeeklyItemVO
+import com.knichu.domain.vo.WeatherWeeklyVO
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
+import javax.inject.Inject
 
 @HiltViewModel
-class ForecastViewModel : BaseViewModel() {
-    // TODO: Implement the ViewModel
+class ForecastViewModel @Inject constructor(
+    private val airPollutionUseCase: AirPollutionUseCase,
+    private val weatherUseCase: WeatherUseCase,
+    private val dataStoreUseCase: DataStoreUseCase
+) : BaseViewModel() {
+    private val _selectedCity: MutableLiveData<String> = MutableLiveData("서울")
+    val selectedCity: LiveData<String> = _selectedCity
+
+    private val _isCityChecked: MutableLiveData<Boolean> = MutableLiveData(false)
+    val isCityChecked: LiveData<Boolean> = _isCityChecked
+
+    private val _lonLat: MutableLiveData<Pair<Double, Double>> = MutableLiveData()
+    val lonLat: LiveData<Pair<Double, Double>> = _lonLat
+
+    private val _weatherNowData: MutableLiveData<WeatherNowVO> = MutableLiveData()
+    val weatherNowData: LiveData<WeatherNowVO> = _weatherNowData
+
+    private val _weather24HourData: MutableLiveData<List<Weather24HourItemVO>> = MutableLiveData()
+    val weather24HourData: LiveData<List<Weather24HourItemVO>> = _weather24HourData
+
+    private val _weatherWeeklyData: MutableLiveData<List<WeatherWeeklyItemVO>> = MutableLiveData()
+    val weatherWeeklyData: LiveData<List<WeatherWeeklyItemVO>> = _weatherWeeklyData
+
+    private val _sunriseSunsetData: MutableLiveData<SunriseSunsetVO> = MutableLiveData()
+    val sunriseSunsetData: LiveData<SunriseSunsetVO> = _sunriseSunsetData
+
+    private val _weatherOtherInfoData: MutableLiveData<WeatherOtherInfoVO> = MutableLiveData()
+    val weatherOtherInfoData: LiveData<WeatherOtherInfoVO> = _weatherOtherInfoData
+
+    private val _weatherForecastTextData: MutableLiveData<WeatherForecastTextVO> = MutableLiveData()
+    val weatherForecastTextData: LiveData<WeatherForecastTextVO> = _weatherForecastTextData
+
+    private val _isAppBarExpanded: MutableLiveData<Boolean> = MutableLiveData()
+    val isAppBarExpanded: LiveData<Boolean> = _isAppBarExpanded
+
+    private val _isRefreshing: MutableLiveData<Boolean> = MutableLiveData()
+    val isRefreshing: LiveData<Boolean> = _isRefreshing
+
+    fun setAppBarExpanded(isExpanded: Boolean) {
+        _isAppBarExpanded.value = isExpanded
+    }
+
+    fun setRefreshing(isRefreshing: Boolean) {
+        _isRefreshing.value = isRefreshing
+    }
+
+    fun changeCityChecked() {
+        _isCityChecked.value = !(isCityChecked.value?: true)
+    }
+
+    fun updateLocationFetchData(lon: Double, lat: Double) {
+        _lonLat.value = Pair(lon, lat)
+        fetchData()
+    }
+
+    private fun fetchData() {
+        if (_isCityChecked.value == true) {
+            weatherUseCase.getNewData()
+            fetchCityNowData(_selectedCity.value?: DEFAULT_CITY)
+            fetchCity24HourData(_selectedCity.value?: DEFAULT_CITY)
+            fetchCityWeeklyData(_selectedCity.value?: DEFAULT_CITY)
+            fetchCitySunriseSunsetData(_selectedCity.value?: DEFAULT_CITY)
+//            fetchCityOtherInfoData(_selectedCity.value?: DEFAULT_CITY)
+//            fetchCityForecastTextData(_selectedCity.value?: DEFAULT_CITY)
+        } else {
+            weatherUseCase.getNewData()
+            fetchCurrentNowData(_lonLat.value?: Pair(DEFAULT_LON, DEFAULT_LAT))
+            fetchCurrent24HourData(_lonLat.value?: Pair(DEFAULT_LON, DEFAULT_LAT))
+            fetchCurrentWeeklyData(_lonLat.value?: Pair(DEFAULT_LON, DEFAULT_LAT))
+            fetchCurrentSunriseSunsetData(_lonLat.value?: Pair(DEFAULT_LON, DEFAULT_LAT))
+//            fetchCurrentOtherInfoData(_lonLat.value?: Pair(DEFAULT_LON, DEFAULT_LAT))
+//            fetchCurrentForecastTextData(_lonLat.value?: Pair(DEFAULT_LON, DEFAULT_LAT))
+        }
+    }
+
+    private fun fetchCurrentNowData(lonLat: Pair<Double, Double>) {
+        weatherUseCase.getCurrentPositionWeatherNow(lonLat.first, lonLat.second)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .toObservable()
+            .bind(_weatherNowData)
+    }
+
+    private fun fetchCurrent24HourData(lonLat: Pair<Double, Double>) {
+        weatherUseCase.getCurrentPositionWeather24Hour(lonLat.first, lonLat.second)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .toObservable()
+            .map{ it.item?: emptyList() }
+            .bind(_weather24HourData)
+    }
+
+    private fun fetchCurrentWeeklyData(lonLat: Pair<Double, Double>) {
+        weatherUseCase.getCurrentPositionWeatherWeekly(lonLat.first, lonLat.second)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .toObservable()
+            .map{ it.item?: emptyList() }
+            .bind(_weatherWeeklyData)
+    }
+
+    private fun fetchCurrentSunriseSunsetData(lonLat: Pair<Double, Double>) {
+        weatherUseCase.getCurrentPositionSunriseSunset(lonLat.first, lonLat.second)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .toObservable()
+            .bind(_sunriseSunsetData)
+    }
+
+    private fun fetchCurrentOtherInfoData(lonLat: Pair<Double, Double>) {
+        weatherUseCase.getCurrentPositionWeatherOtherInfo(lonLat.first, lonLat.second)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .toObservable()
+            .bind(_weatherOtherInfoData)
+    }
+
+    private fun fetchCurrentForecastTextData(lonLat: Pair<Double, Double>) {
+        weatherUseCase.getCurrentPositionWeatherForecastText(lonLat.first, lonLat.second)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .toObservable()
+            .bind(_weatherForecastTextData)
+    }
+
+
+    private fun fetchCityNowData(city: String) {
+        weatherUseCase.getCityPositionWeatherNow(city)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .toObservable()
+            .bind(_weatherNowData)
+    }
+
+    private fun fetchCity24HourData(city: String) {
+        weatherUseCase.getCityPositionWeather24Hour(city)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .toObservable()
+            .map{ it.item?: emptyList() }
+            .bind(_weather24HourData)
+    }
+
+    private fun fetchCityWeeklyData(city: String) {
+        weatherUseCase.getCityPositionWeatherWeekly(city)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .toObservable()
+            .map{ it.item?: emptyList() }
+            .bind(_weatherWeeklyData)
+    }
+
+    private fun fetchCitySunriseSunsetData(city: String) {
+        weatherUseCase.getCityPositionSunriseSunset(city)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .toObservable()
+            .bind(_sunriseSunsetData)
+    }
+
+    private fun fetchCityOtherInfoData(city: String) {
+        weatherUseCase.getCityPositionWeatherOtherInfo(city)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .toObservable()
+            .bind(_weatherOtherInfoData)
+    }
+
+    private fun fetchCityForecastTextData(city: String) {
+        weatherUseCase.getCityPositionWeatherForecastText(city)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .toObservable()
+            .bind(_weatherForecastTextData)
+    }
+
+    companion object {
+        private const val DEFAULT_CITY = "서울"
+        private const val DEFAULT_LON = 126.9778
+        private const val DEFAULT_LAT = 37.5683
+    }
+
 }
