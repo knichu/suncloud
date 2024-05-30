38a631ee6eba7bad6036adb09855bcd71743128a
knichu <leonard1219@naver.com>
2024-05-30 15:55:31 +0900

    feat : AirPollution에 대한 Parser 및 DataVO 설계
    필요한 형태의 데이터로 앱 내부에서 가공하기 위해 설계

---
+++ domain/src/main/java/com/knichu/domain/vo/AirPollutionDataVO.kt
@@ -0,0 +1,8 @@
+package com.knichu.domain.vo
+
+data class AirPollutionDataVO(
+    val pm2_5Density: String? = null,
+    val pm10Density: String? = null,
+    val pm2_5Quality: String? = null,
+    val pm10Quality: String? = null
+)
