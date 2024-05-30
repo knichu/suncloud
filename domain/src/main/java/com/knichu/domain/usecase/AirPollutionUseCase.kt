142c6f85019ed8772858ef30a7f8bc331c7dc705
knichu <leonard1219@naver.com>
2024-05-30 15:56:48 +0900

    refactor : AirPollutionUseCase 수정
    Parser로 Data를 변환함에 따라 수정

--- domain/src/main/java/com/knichu/domain/usecase/AirPollutionUseCase.kt
+++ domain/src/main/java/com/knichu/domain/usecase/AirPollutionUseCase.kt
@@ -1,10 +1,11 @@
 package com.knichu.domain.useCase
 
+import com.knichu.domain.vo.AirPollutionDataVO
 import com.knichu.domain.vo.AirPollutionVO
 import io.reactivex.rxjava3.core.Single
 
 interface AirPollutionUseCase {
-    fun getCurrentPositionAirPollution(lon: Double, lat: Double): Single<AirPollutionVO>
+    fun getCurrentPositionAirPollution(lon: Double, lat: Double): Single<AirPollutionDataVO>
 
-    fun getCityPositionAirPollution(city: String): Single<AirPollutionVO>
+    fun getCityPositionAirPollution(city: String): Single<AirPollutionDataVO>
 }
--- domain/src/main/java/com/knichu/domain/usecase/useCaseImpl/AirPollutionUseCaseImpl.kt
+++ domain/src/main/java/com/knichu/domain/usecase/useCaseImpl/AirPollutionUseCaseImpl.kt
@@ -5,6 +5,8 @@ import com.knichu.domain.param.AirPollutionRequestParam
 import com.knichu.domain.repository.AirPollutionRepository
 import com.knichu.domain.repository.CityLocationRepository
 import com.knichu.domain.useCase.AirPollutionUseCase
+import com.knichu.domain.util.AirPollutionParser
+import com.knichu.domain.vo.AirPollutionDataVO
 import com.knichu.domain.vo.AirPollutionVO
 import io.reactivex.rxjava3.core.Single
 import javax.inject.Inject
@@ -14,28 +16,30 @@ class AirPollutionUseCaseImpl @Inject constructor(
     private val cityLocationRepository: CityLocationRepository,
 ): AirPollutionUseCase {
     private val allCityList = cityLocationRepository.getAllCityLocation()
-    private val openWeatherMapApiKey = BuildConfig.OPEN_WEATHER_MAP_API_KEY
 
-    override fun getCurrentPositionAirPollution(lon: Double, lat: Double): Single<AirPollutionVO> {
-        return airPollutionRepository.getAirPollution(
+    override fun getCurrentPositionAirPollution(lon: Double, lat: Double): Single<AirPollutionDataVO> {
+        val airPollutionVO = airPollutionRepository.getAirPollution(
             AirPollutionRequestParam(
                 lon = lon,
                 lat = lat
             )
         )
+
+        return AirPollutionParser.getAirPollutionVO(airPollutionVO)
     }
 
-    override fun getCityPositionAirPollution(city: String): Single<AirPollutionVO> {
+    override fun getCityPositionAirPollution(city: String): Single<AirPollutionDataVO> {
         return allCityList
             .flatMap { cityLocationVO ->
                 val cityLocationItemVO = cityLocationVO.item?.find { it.cityName == city }
-
-                airPollutionRepository.getAirPollution(
+                val airPollutionVO = airPollutionRepository.getAirPollution(
                     AirPollutionRequestParam(
                         lon = cityLocationItemVO?.longitude?.toDouble(),
                         lat = cityLocationItemVO?.latitude?.toDouble()
                     )
                 )
+
+                AirPollutionParser.getAirPollutionVO(airPollutionVO)
             }
     }
 }
