95b23cd3f7e4eeab6ba5fc4a86e943e456ffe158
knichu <leonard1219@naver.com>
2024-08-14 22:22:24 +0900

    feat : Version 수정

--- buildSrc/src/main/java/DefaultConfig.kt
+++ buildSrc/src/main/java/DefaultConfig.kt
@@ -5,5 +5,5 @@ object DefaultConfig {
     const val MIN_SDK_VERSION = 28
     const val TARGET_SDK_VERSION = 33
     const val VERSION_CODE = 1
-    const val VERSION_NAME = "0.1.0"
+    const val VERSION_NAME = "1.0.0"
 }
