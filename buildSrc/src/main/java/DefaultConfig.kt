0f615b8c39fb417df8c188579a9c681b377fb6c2
knichu <leonard1219@naver.com>
2024-09-05 06:08:30 +0900

    feat : SDK & version update

--- buildSrc/src/main/java/DefaultConfig.kt
+++ buildSrc/src/main/java/DefaultConfig.kt
@@ -1,9 +1,9 @@
 object DefaultConfig {
     const val APPLICATION_ID = "com.knichu.suncloud"
-    const val COMPILE_SDK_VERSION = 33
+    const val COMPILE_SDK_VERSION = 34
     const val BUILD_TOOLS_VERSION = "33.0.1"
     const val MIN_SDK_VERSION = 28
-    const val TARGET_SDK_VERSION = 33
-    const val VERSION_CODE = 2
-    const val VERSION_NAME = "1.0.0"
+    const val TARGET_SDK_VERSION = 34
+    const val VERSION_CODE = 4
+    const val VERSION_NAME = "1.0.2"
 }
