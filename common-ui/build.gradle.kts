fe051d69a568375278e807e327808a3fffb81e37
knichu <leonard1219@naver.com>
2024-11-25 03:02:58 +0900

    feat : compose theme 추가
    gradle 설정 같이 추가

--- common-ui/build.gradle.kts
+++ common-ui/build.gradle.kts
@@ -73,6 +73,21 @@ dependencies {
 
     kapt("com.google.dagger:hilt-android-compiler:2.50")
     implementation("com.google.dagger:hilt-android:2.50")
+    
+    // compose
+    implementation ("androidx.navigation:navigation-compose:2.8.4")
+
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.5")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+
 }
 
 kapt {
---
+++ common-ui/src/main/java/com/knichu/common_ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package com.knichu.common_ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val One = Color(0xFFF4BFBF)
+val OneVarient = Color(0xFFA27676)
+val Two = Color(0xFFFFD9C0)
+val Three = Color(0xFFFAF0D7)
+val Four = Color(0xFF8CC0DE)
