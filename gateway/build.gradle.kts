fc7d968e3ec3f8fa5d8e87310259f6905cb0424c
knichu <leonard1219@naver.com>
2026-05-25 02:50:11 +0900

    refactor : gateway 모듈 DataStoreModule 코루틴 DataStore로 전환

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -90,7 +90,6 @@ dependencies {
 
     // DataStore
     implementation("androidx.datastore:datastore-preferences:1.0.0")
-    implementation("androidx.datastore:datastore-preferences-rxjava3:1.0.0")
 }
 
 kapt {
