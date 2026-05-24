fd3b2d1b66cdddeefbd424c32d2e2ddc6e972f0a
knichu <leonard1219@naver.com>
2026-05-25 02:50:01 +0900

    refactor : gateway 모듈 NetworkModule RxJava3CallAdapterFactory 제거

--- gateway/src/main/java/com/knichu/gateway/di/NetworkModule.kt
+++ gateway/src/main/java/com/knichu/gateway/di/NetworkModule.kt
@@ -7,7 +7,6 @@ import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
@@ -40,7 +39,6 @@ object NetworkModule {
             .baseUrl(BASE_URL_OPEN_WEATHER_MAP)
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
-            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
             .build()
     }
 
@@ -52,7 +50,6 @@ object NetworkModule {
             .baseUrl(BASE_URL_WEATHER)
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
-            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
             .build()
     }
 
@@ -64,7 +61,6 @@ object NetworkModule {
             .baseUrl(BASE_URL_OPEN_WEATHER_MAP)
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
-            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
             .build()
     }
 }
