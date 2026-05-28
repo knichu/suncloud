95fea971a6fae6fabb496c4223ece8ba10503112
knichu <leonard1219@naver.com>
2026-05-29 03:24:10 +0900

    feat: setting 모듈 Compose 마이그레이션

--- setting/src/main/java/com/knichu/setting/ui/SettingViewModel.kt
+++ setting/src/main/java/com/knichu/setting/ui/SettingViewModel.kt
@@ -1,8 +1,6 @@
 package com.knichu.setting.ui
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import com.knichu.common.base.BaseViewModel
+import com.knichu.common.base.BaseMviViewModel
 import com.knichu.domain.useCase.DataStoreUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -10,7 +8,9 @@ import javax.inject.Inject
 @HiltViewModel
 class SettingViewModel @Inject constructor(
     private val dataStoreUseCase: DataStoreUseCase
-) : BaseViewModel() {
-    private val _data: MutableLiveData<String> = MutableLiveData()
-    val data: LiveData<String> = _data
+) : BaseMviViewModel<SettingUiIntent, SettingUiState, SettingUiEffect>() {
+
+    override fun initialState() = SettingUiState()
+
+    override fun handleIntent(intent: SettingUiIntent) {}
 }
