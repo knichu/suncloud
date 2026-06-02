7b5fe2ecc36a697bc8932306af2e900eba6f3fa0
knichu <leonard1219@naver.com>
2026-06-02 13:33:01 +0900

    feat: 세팅 탭 온도 단위(°C/°F) 설정 UI 구현

--- setting/src/main/java/com/knichu/setting/ui/SettingViewModel.kt
+++ setting/src/main/java/com/knichu/setting/ui/SettingViewModel.kt
@@ -1,8 +1,11 @@
 package com.knichu.setting.ui
 
+import androidx.lifecycle.viewModelScope
 import com.knichu.common.base.BaseMviViewModel
+import com.knichu.domain.constants.WeatherTempUnit
 import com.knichu.domain.useCase.DataStoreUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -12,5 +15,27 @@ class SettingViewModel @Inject constructor(
 
     override fun initialState() = SettingUiState()
 
-    override fun handleIntent(intent: SettingUiIntent) {}
+    init {
+        loadTempUnit()
+    }
+
+    override fun handleIntent(intent: SettingUiIntent) {
+        when (intent) {
+            is SettingUiIntent.OnTempUnitChanged -> saveTempUnit(intent.unit)
+        }
+    }
+
+    private fun loadTempUnit() {
+        viewModelScope.launch {
+            val unit = dataStoreUseCase.getUserTempUnit()
+            setState { copy(tempUnit = if (unit == WeatherTempUnit.NONE) WeatherTempUnit.CELSIUS else unit) }
+        }
+    }
+
+    private fun saveTempUnit(unit: WeatherTempUnit) {
+        setState { copy(tempUnit = unit) }
+        viewModelScope.launch {
+            dataStoreUseCase.storeUserTempUnit(unit)
+        }
+    }
 }
